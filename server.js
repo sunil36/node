35c71d9c035fe4604c5f809eb5e9a21b4c03c253
cc_syadav <syadav@vivaindiagroup.in>
2021-08-17 10:26:34 -0700

    changes add

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+const ejs=require("ejs");
+const path=require("path");
+const expressLayout=require("express-ejs-layouts");
+const port =process.env.port|| 3000;
+app.get('/',(reqestObject,responseObject)=>{
+    responseObject.render("home");
+});
+// set layout 
+app.use(expressLayout);
+app.set('views',path.join(__dirname,"/resources/views"));
+app.set('view engine','ejs');
+// set layout end 
+
+app.listen(port,()=>{
+    console.log(`listing to port ${port}`);
+})
