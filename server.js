0f0f04ddf65cdb7e5d8bdf0851228bd8ba824559
cc_syadav <syadav@vivaindiagroup.in>
2021-08-18 09:11:26 -0700

    second day

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const ejs=require("ejs");
 const path=require("path");
 const expressLayout=require("express-ejs-layouts");
 const port =process.env.port|| 3000;
+app.use(express.static('public'));
+
 app.get('/',(reqestObject,responseObject)=>{
     responseObject.render("home");
 });
@@ -12,7 +14,6 @@ app.use(expressLayout);
 app.set('views',path.join(__dirname,"/resources/views"));
 app.set('view engine','ejs');
 // set layout end 
-
 app.listen(port,()=>{
     console.log(`listing to port ${port}`);
 })
