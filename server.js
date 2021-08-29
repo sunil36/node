9a89da8ce3642d9fa0af8846bc012de91aba0fdf
cc_syadav <syadav@vivaindiagroup.in>
2021-08-29 00:21:47 -0700

    New File Uploaded

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+require("dotenv").config();
 const express=require("express");
 const app=express();
 const ejs=require("ejs");
 const path=require("path");
 const expressLayout=require("express-ejs-layouts");
- const mongoose=require("mongoose");
+const session =require("express-session");
+const flash =require("express-flash");
+const mongoose=require("mongoose");
+const MongoDBStore=require("connect-mongo")(session);
 const port =process.env.port|| 3000;
 app.use(express.static('public'));
+
 // Database connection
 const URL='mongodb://localhost/pizza'
 mongoose.connect(URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
@@ -15,6 +20,22 @@ connection.once('open', () => {
 }).catch(err => {
     console.log('Connection failed...')
 });
+//config session store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+// config session here
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  store: new MongoDBStore({
+    mongooseConnection: mongoose.connection
+}),
+  saveUninitialized: false,
+  cookie: { maxAge:1000*60*60*24  }
+}))
+app.use(flash());
 // set layout 
 app.use(expressLayout);
 app.set('views',path.join(__dirname,"/resources/views"));
